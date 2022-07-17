8fec9f8373d2cdd00879167d5700464108651430
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-17 21:17:46 +0100

    Create and connect oscillator to audio output

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import "./App.scss"
 
 let audioContext = new (window.AudioContext || window.webkitAudioContext)()
 let out = audioContext.destination
+let osc1 = audioContext.createOscillator()
+let gain1 = audioContext.createGain()
+osc1.connect(gain1)
+gain1.connect(out)
+gain1.gain.value = 0.01
 function App() {
   return (
     <div className='App'>

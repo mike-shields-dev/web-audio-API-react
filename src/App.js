659f1429bace35e61298c91962213974ade09a88
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-17 21:11:28 +0100

    Initialise Audio Context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.scss"
 
+let audioContext = new (window.AudioContext || window.webkitAudioContext)()
+let out = audioContext.destination
 function App() {
   return (
     <div className='App'>
